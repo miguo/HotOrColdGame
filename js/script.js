5a8e6358b228a20d8d975d7613de2ee764f802a8
Michelle <MichelleGuo@Michelles-MacBook-Air.local>
2013-12-25 11:45:39 +0800

    First attempt at game
    First attempt at writing the Javascript to run the game. When
    Javascript is uncommented and run, the rectangle doesn’t show and the
    game doesn’t work.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,59 @@
 $(document).ready(function() {
 
 	var paper = new Raphael(document.getElementById('myArc'), 400, 80);
-	var rectangle = paper.rect(60, 26, 280, 20, 5);
-
-
-
-//Changes feedback-text based on difference between guess and answer
-
-
+	var rectangle = paper.rect(60, 26, 280, 20, 5).attr({fill: '#2c3e50', stroke: '#2c3e50'});
 	
-//sample of changing the color meter
-//	if (guessNumber === randomNumber) {
-//	
-//	}
+/*	var randomNumber = Math.ceil(Math.random()*100);
+	console.log(randomNumber);
 
-	rectangle.attr({
-        'fill': '#2c3e50',
-        'stroke': '#2c3e50'
-    });
-    
-    var clicked = false;
-    
-    rectangle.click(function() {
-        if (clicked) {
-            rectangle.attr({
-                'fill': '#2c3e50',
-                'stroke': '#2c3e50'
-            });
-        } else {
-            rectangle.attr({
-                'fill': '#2980b9',
-                'stroke': '#2980b9'
-            });
-        }
-        clicked = !clicked;
-    });
+	$(".button-submit").click(playGame);
 
 
+//Play game function
+function playGame() {
+	var guessNumber = +$("input:first").val();
+	var numDiff = Math.abs(randomNumber - guessNumber);
+
+	if (guessNumber < 1 || guessNumber > 100) {
+		alert("Your guess must be between 1 and 100. Try again.")
+		return false;
+	}
+
+	else if (guessNumber === randomNumber) {
+		$('.feedback-text').text("You win!");
+		rectangle.attr({
+			'fill': '#f1c40f',
+			'stroke': '#f1c40f'
+		});
+	}
+
+	else if (numDiff > 0 && numDiff <= 15) {
+		$('.feedback-text').text("You are getting hotter!");
+		rectangle.attr({
+			'fill': '#e74c3c',
+			'stroke': '#e74c3c'
+		});
+	}
+
+	else if (numDiff >= 16 && numDiff <= 40) {
+		$('.feedback-text').text("You are getting warmer.");
+		rectangle.attr({
+			'fill': '#e67e22',
+			'stroke': '#e67e22'
+		});
+	}
+
+	else (numDiff >= 41 && numDiff <= 99) {
+		$('.feedback-text').text("You are getting colder.");
+		rectangle.attr({
+			'fill': '#2980b9',
+			'stroke': '#2980b9'
+		});
+	}
+
+}
+
+*/
 
 });
+
