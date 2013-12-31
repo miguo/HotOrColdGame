5b51f82224e89a8c8dcc94a60e92c3d09a8f8675
Michelle <MichelleGuo@Michelles-MacBook-Air.local>
2013-12-31 12:36:32 +0800

    Javascript game 1.3
    Got rid of comma after last number

--- js/script.js
+++ js/script.js
@@ -4,15 +4,18 @@ $(document).ready(function() {
 	var rectangle = paper.rect(60, 26, 280, 20, 5).attr({'fill': '#2c3e50', 'stroke': '#2c3e50'}); 
 	var randomNumber = Math.floor((Math.random()*100) + 1);
 
+
 	//Generate a random number
  	function getRandomNumber() {
 		console.log("This round's random number is " + randomNumber);
 	}
 
+
 	//New game upon page reload
 	window.onload = function(){
 		getRandomNumber();
 	}
+
 
 	//Event handler to allow users to submit guess using Enter key
 	$('input').bind('keypress', function(e) {
@@ -22,6 +25,7 @@ $(document).ready(function() {
 		document.getElementById('guess-here').reset();
 	}
 	});
+
 
 	//Play game function
 	document.getElementById('button-submit').onclick = function playGame() {
@@ -66,9 +70,16 @@ $(document).ready(function() {
 	$('#previousGuesses').hide();
 	$('#button-submit').click(function() {
 		var guessNumber = $("#guess-box").val();
+		var numDiff = Math.abs(randomNumber - guessNumber);
 		$('#previousGuesses').show();
-	//Note: How do I get the last number to not have the comma?
-		$('#previousGuesses').append(guessNumber + ", ");
+		if (numDiff === 0) {
+			$('#previousGuesses').append(guessNumber);
+		}
+
+		else {
+			$('#previousGuesses').append(guessNumber + ", ");
+		}
+
 	});
 
 
